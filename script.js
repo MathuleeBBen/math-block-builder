230e369505c892d1eb8ff6557f8b7a5508776861
MathuleeBBen <mathuleeben@gmail.com>
2026-07-07 10:17:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Math Block Builder v1.0");
+let score = 0;
+const correctAnswer = 7;
+
+const blocks = document.querySelectorAll(".choices .block");
+const scoreBox = document.querySelector(".topbar div");
+
+blocks.forEach(block => {
+    block.style.cursor = "pointer";
+
+    block.addEventListener("click", () => {
+        const value = Number(block.textContent);
+
+        if (value === correctAnswer) {
+            score += 10;
+            scoreBox.textContent = "⭐ คะแนน : " + score;
+            alert("🎉 ตอบถูก!");
+        } else {
+            alert("❌ ลองอีกครั้ง");
+        }
+    });
+});
